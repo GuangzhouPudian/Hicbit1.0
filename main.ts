587f3d9b1656cb5548f1fadf0a288ae9f2d0939d
Albert <926190086@139.com>
2021-06-18 21:04:47 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -226,7 +226,7 @@ namespace hicbit {
             buf[i+4] = str.charCodeAt(i);
         buf[len+4] = 0xef;
         serial.writeBuffer(buf);
-        basic.pause(300);
+        basic.pause(200);
     }
 
     //% row.min=1 row.max=7
@@ -250,7 +250,7 @@ namespace hicbit {
         buf[len + 5] = 0x20;
         buf[len + 6] = 0xef;
         serial.writeBuffer(buf);
-        basic.pause(300);
+        basic.pause(200);
     }
 
     //% row.min=1 row.max=7
@@ -276,7 +276,7 @@ namespace hicbit {
         buf[len + 5] = 0x20;
         buf[len + 6] = 0xef;
         serial.writeBuffer(buf);
-        basic.pause(300);
+        basic.pause(200);
     }
 
     //% row1.min=1 row1.max=7
@@ -300,7 +300,7 @@ namespace hicbit {
         }
         buf[4] = 0xef;
         serial.writeBuffer(buf);
-        basic.pause(300);
+        basic.pause(200);
     }
 
     //% weight=40 block="方向键|%directkey按下"
